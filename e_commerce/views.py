fe261eb4aac1c581239f34c69a9909f543ff5491
Erisvaldo <erisvaldocarvalhosilva@gmail.com>
2019-05-31 21:12:29 -0300

    edição no arquivo template\contact\view.html e e_commerce\views.py e criação do arquivo e_commerce\forms.py

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 def home_page(request):
     context = {
@@ -17,11 +18,17 @@ def about_page(request):
     return render(request, "about/view.html", context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Página contato",
-        "content": "Bem-vindo a página contato"
+        "content": "Bem-vindo a página contato",
+        "form":contact_form
     }
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
 
     if request.method == "POST":
         print(request.POST)
+        print(request.POST.get('nome_completo'))
     return render(request, "contact/view.html", context)
